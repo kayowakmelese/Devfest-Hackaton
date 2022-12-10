d9ce43418d26d0b5fee3f7ea6b1cb5adf59fa9b4
kayowakmelese <kayomelese4@gmail.com>
2022-12-09 16:58:59 -0800

    first template

--- navigation/index.js
+++ navigation/index.js
@@ -8,9 +8,9 @@ const Tab = createBottomTabNavigator();
 
 return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator >
+        {/* <Tab.Screen name="Home" component={HomeScreen} /> */}
+        {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
       </Tab.Navigator>
     </NavigationContainer>
   );
